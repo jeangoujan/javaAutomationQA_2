173a0cf0ab8b1729f890224c3e2b2b9d97ef2eeb
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2026-01-14 14:08:33 +0500

    added: iframe, and different launch windows test

--- app/src/test/java/ui/waitsTest.java
+++ app/src/test/java/ui/waitsTest.java
@@ -1,7 +1,9 @@
 package ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -11,14 +13,15 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -112,5 +115,136 @@ public class waitsTest {
         Cookie updatedCookie = driver.manage().getCookieNamed("username");
 
         assertNotEquals(cookie.getValue(), updatedCookie.getValue());   
+    }
+
+    @Test
+    void iFramesTest(){
+        String endpoint = "iframes.html";
+        driver.get(BASE_URL + endpoint);
+        
+        WebElement iframe = driver.findElement(By.id("my-iframe"));
+        driver.switchTo().frame(iframe);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("p")));
+
+        assertTrue(driver.findElement(By.xpath("(//p)[1]")).isDisplayed());
+        driver.switchTo().defaultContent();
+    }
+
+    @Test
+    void launchAlertTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        driver.findElement(By.id("my-alert")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.accept();
+        assertThrows(NoAlertPresentException.class, () -> driver.switchTo().alert());
+    }
+
+    @Test
+    void launchConfirmOkTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-confirm")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.accept();
+        assertTrue(driver.findElement(By.xpath("//p[text()='You chose: true']")).isDisplayed());
+    }
+
+    @Test
+    void launchConfirmCancelTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-confirm")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.dismiss();
+        assertTrue(driver.findElement(By.xpath("//p[text()='You chose: false']")).isDisplayed());
+    }
+
+    @Test
+    void launchPromptWithTextTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-prompt")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.sendKeys("Test");
+        alert.accept();
+
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'You typed:')]")).isDisplayed());
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'Test')]")).isDisplayed());
+
+    }
+
+    @Test
+    void launchPromptCancelTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-prompt")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.dismiss();
+
+        assertTrue(driver.findElement(By.xpath("//p[text()='You typed: null']")).isDisplayed());
+    }
+
+    @Test
+    void launchPromptEmptyStringTest(){
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-prompt")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+        alert.accept();
+
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'You typed:')]")).isDisplayed());
+    }
+
+    @Test
+    void launchModalSaveChangesTest() throws InterruptedException{
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-modal")).click();
+        
+        wait.until((ExpectedConditions.presenceOfElementLocated(By.className("modal-content"))));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space(.)='Save changes']")));
+        driver.findElement(By.xpath("//button[normalize-space(.)='Save changes']")).click();
+        
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'You chose:')]")).isDisplayed());
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'Save changes')]")).isDisplayed());
+    }
+
+    @Test
+    void launchModalCloseTest() throws InterruptedException{
+        String endpoint = "dialog-boxes.html";
+        driver.get(BASE_URL + endpoint); 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.id("my-modal")).click();
+        
+        wait.until((ExpectedConditions.presenceOfElementLocated(By.className("modal-content"))));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space(.)='Close']")));
+        driver.findElement(By.xpath("//button[normalize-space(.)='Close']")).click();
+        
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'You chose:')]")).isDisplayed());
+        assertTrue(driver.findElement(By.xpath("//p[contains(text(), 'Close')]")).isDisplayed());
     }
 }
