a80c509090de36eaa0bcb53bc664bbd4ef5c58b4
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2026-01-24 00:48:57 +0500

    add remote url

--- app/src/test/java/ui/BaseTest.java
+++ app/src/test/java/ui/BaseTest.java
@@ -1,11 +1,18 @@
 package ui;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.charset.MalformedInputException;
 import java.time.Duration;
+import java.util.Map;
+
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 class BaseTest {
     WebDriver driver;
@@ -13,7 +20,7 @@ class BaseTest {
     
     @BeforeEach
     void setup(){
-        driver = new ChromeDriver();
+        initDriver();
         driver.get(BASE_URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
@@ -22,5 +29,26 @@ class BaseTest {
     @AfterEach
     void tearDown(){
         driver.quit();
+    }
+
+    private void initDriver(){
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        System.out.println("SELENIUM_REMOTE_URL = " + remoteUrl);
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                URI uri = URI.create(remoteUrl);
+                driver = new RemoteWebDriver(uri.toURL(), options);
+            } catch(MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e); 
+            }
+        } else{
+            driver = new ChromeDriver();
+        }
     }
 }
