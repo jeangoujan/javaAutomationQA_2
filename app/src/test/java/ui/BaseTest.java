2c61d17a81874b5ed64b3064206961555178f8ff
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2026-01-26 18:00:09 +0500

    fixing build.gradle

--- app/src/test/java/ui/BaseTest.java
+++ app/src/test/java/ui/BaseTest.java
@@ -2,10 +2,7 @@ package ui;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.nio.charset.MalformedInputException;
 import java.time.Duration;
-import java.util.Map;
-
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,46 +14,41 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import io.qameta.allure.Allure;
 
 class BaseTest {
-    WebDriver driver;
-    protected static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
-    
+
+    protected WebDriver driver;
+    protected static final String BASE_URL =
+            "https://bonigarcia.dev/selenium-webdriver-java/";
+
     @BeforeEach
-    void setup() throws MalformedURLException{
+    void setup() throws MalformedURLException {
         initDriver();
         driver.get(BASE_URL);
-        if (!(driver instanceof RemoteWebDriver)) {
-            driver.manage().window().maximize();
-        }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
     @AfterEach
-    void tearDown(){
-            if (driver != null) {
-        try {
-            driver.quit();
-        } catch (Exception ignored) {
+    void tearDown() {
+        if (driver != null) {
+            try {
+                driver.quit();
+            } catch (Exception ignored) {
+            }
         }
     }
-    }
 
-    private void initDriver() {
+    private void initDriver() throws MalformedURLException {
         String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
-        Allure.addAttachment("remote", String.valueOf(remoteUrl));
+        Allure.addAttachment("SELENIUM_REMOTE_URL", String.valueOf(remoteUrl));
 
-        if (remoteUrl != null && !remoteUrl.isEmpty()) {
-            try {
-                ChromeOptions options = new ChromeOptions();
-                options.setCapability("browserName", "chrome");
-                options.setCapability("platformName", "LINUX");
+        if (remoteUrl != null && !remoteUrl.isBlank()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
 
-                driver = new RemoteWebDriver(
+            driver = new RemoteWebDriver(
                     URI.create(remoteUrl).toURL(),
                     options
-                );
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("Bad Selenium URL", e);
-            }
+            );
         } else {
             driver = new ChromeDriver();
         }
